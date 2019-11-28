468b1c457be5defedc87989f16422c074faf8bdd
KiraGamesBR <romuloevil@outlook.com>
2019-11-28 14:04:08 -0300

    Implementando View.categorias.show

--- resources/views/categorias/show.blade.php
+++ resources/views/categorias/show.blade.php
@@ -4,3 +4,35 @@
         Inserir produto
     </a>
 </button>
+<table border="1">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Cor</th>
+            <th>Qtde</th>
+            <th>Valor</th>
+            <th>Foto</th>
+            <th>Descrição</th>
+            <th>ID da categoria</th>
+            <th>Ultima atualização</th>
+            <th>Data de Cadastro</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($categoria->produtos as $value)
+        <tr>
+            <td>{{$value->id}}</td>
+            <td>{{$value->nome}}</td>
+            <td>{{$value->cor}}</td>
+            <td>{{$value->qtde}}</td>
+            <td>{{$value->valor}}</td>
+            <td>{{$value->foto}}</td>
+            <td>{{$value->descricao}}</td>
+            <td>{{$value->categoria_id}}</td>
+            <td>{{$value->updated_at}}</td>
+            <td>{{$value->created_at}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
